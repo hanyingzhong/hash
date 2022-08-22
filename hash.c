6eaabc43335215298556425430870e1554b63280
hanyingzhong <2655942208@qq.com>
2022-08-22 17:02:26 +0800

    Update hash.c

--- hash.c
+++ hash.c
@@ -11,42 +11,43 @@
  * Set hash `key` to `val`.
  */
 
-inline void
-hash_set(hash_t *self, const char *key, void *val) {
-  int ret;
-  khiter_t k = kh_put(ptr, self, key, &ret);
-  kh_value(self, k) = val;
+void rpc_hash_set (rpc_hash_t *self, const char *key, void *val)
+{
+    int ret;
+    khiter_t k = kh_put(ptr, self, key, &ret);
+    kh_value(self, k) = val;
 }
 
 /*
  * Get hash `key`, or NULL.
  */
 
-inline void *
-hash_get(hash_t *self, const char *key) {
-  khiter_t k = kh_get(ptr, self, key);
-  return k == kh_end(self) ? NULL : kh_value(self, k);
+void *rpc_hash_get (rpc_hash_t *self, const char *key)
+{
+    khiter_t k = kh_get(ptr, self, key);
+    return k == kh_end(self) ? NULL : kh_value(self, k);
 }
 
 /*
  * Check if hash `key` exists.
  */
 
-inline int
-hash_has(hash_t *self, const char *key) {
-  if(!hash_size(self)) return 0;
-  khiter_t k = kh_get(ptr, self, key);
-  return k != kh_end(self);
+int rpc_hash_has (rpc_hash_t *self, const char *key)
+{
+    if (!rpc_hash_size (self))
+        return 0;
+    khiter_t k = kh_get(ptr, self, key);
+    return k != kh_end(self);
 }
 
 /*
  * Remove hash `key`.
  */
 
-void
-hash_del(hash_t *self, const char *key) {
-  khiter_t k = kh_get(ptr, self, key);
-  kh_del(ptr, self, k);
+void rpc_hash_del (rpc_hash_t *self, const char *key)
+{
+    khiter_t k = kh_get(ptr, self, key);
+    kh_del(ptr, self, k);
 }
 
 // tests
